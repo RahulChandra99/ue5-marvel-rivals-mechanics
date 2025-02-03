a9e58a741c0faefbaeaa277f263b3ef4c39bfc56
Rahul Chandra <60950973+RahulChandra99@users.noreply.github.com>
2025-02-03 17:24:04 +0000

    project setup and character import

---
+++ Source/MarvelRivalsGAS/MarvelRivalsGAS.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MarvelRivalsGAS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MarvelRivalsGAS, "MarvelRivalsGAS" );
+ 
--- Source/marvel_rivals_gas/marvel_rivals_gas.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "marvel_rivals_gas.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, marvel_rivals_gas, "marvel_rivals_gas" );
- 
